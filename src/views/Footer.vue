306f55cf8949f577c32978283b46efa0d56f08af
poteitoo <1105tigery@gmail.com>
2021-01-17 22:02:21 +0900

    Add footer & header

---
+++ src/views/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-footer app class="footer"><p>world</p></v-footer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class Footer extends Vue {}
+</script>
---
+++ src/views/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="header" id="nav">
+    <p>hello</p>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss" scope>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>

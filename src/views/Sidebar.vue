3154b9bdad4e14d72c304204f18c0870dfcb872d
poteitoo <1105tigery@gmail.com>
2021-01-18 01:30:34 +0900

    Fix element name

---
+++ src/views/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-navigation-drawer permanent app>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="title"> Application </v-list-item-title>
+        <v-list-item-subtitle> subtext </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense nav>
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {
+  items: { [title: string]: string }[] = [
+    { title: "活動記録" },
+    { title: "お知らせ" },
+    { title: "NSSについて" },
+    { title: "NSS運営サイト" },
+    { title: "お問い合わせ" },
+  ];
+}
+</script>

2affdea6e76e086d8f476d01d2f4cdbd6207e706
poteitoo <1105tigery@gmail.com>
2021-01-18 15:43:24 +0900

    Add markdown

---
+++ src/components/MarkDownText.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-html="compiledMarkdown"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import marked from "marked";
+
+// marked.setOptions({
+//   highlight: function (code: any, lang: any) {
+//     return highlightjs.highlightAuto(code, [lang]).value;
+//   }, // シンタックスハイライトに使用する関数の設定
+//   pedantic: false, // trueの場合はmarkdown.plに準拠する gfmを使用する場合はfalseで大丈夫
+//   gfm: true, // GitHub Flavored Markdownを使用
+//   breaks: true, // falseにすると改行入力は末尾の半角スペース2つになる
+//   sanitize: true, // trueにすると特殊文字をエスケープする
+//   silent: false, // trueにするとパースに失敗してもExceptionを投げなくなる
+// });
+
+@Component
+export default class MarkDownText extends Vue {
+  @Prop() private markdown!: string;
+  private compiledMarkdown: string = marked(this.markdown);
+  //   private get compiledMarkdown(): string {
+  //     return marked(this.markdown, { sanitize: true });
+  //   }
+}
+</script>
---
+++ src/views/Top.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="top">
+    <img
+      alt="nss top picture"
+      src="../assets/privates/images/cropped-header.jpg"
+    />
+    <MarkDownText :markdown="content" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import MarkDownText from "@/components/MarkDownText.vue";
+@Component({
+  components: { MarkDownText },
+})
+export default class Home extends Vue {
+  private content = `
+  ## hello
+  # world
+  `;
+}
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+}
+</style>

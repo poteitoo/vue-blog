00edd29bcdb4cab0066d3601346db5c5cc5ef8a8
poteitoo <1105tigery@gmail.com>
2021-01-21 01:13:37 +0900

    Add importing mardown file logics

--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>winners' info</h1>
+  <div class="rules">
+    <h1>ISEFのルール</h1>
+    <MarkDownText :markdown="content" />
+    <div :v-html="content"></div>
+  </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import MarkDownText from "@/components/MarkDownText.vue";
+import docs from "@/assets/privates/docs/rules.md";
+
+@Component({
+  components: { MarkDownText },
+})
+export default class Rules extends Vue {
+  private content = docs;
+}
+</script>
